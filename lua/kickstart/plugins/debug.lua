01e12ddddea734258155c99e31e67a94f875dbd2
Stefanos Valoumas <svaloumas@gmail.com>
2024-12-09 11:20:32 +0200

    add debug shortcuts

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -33,6 +33,9 @@ return {
       { '<F1>', dap.step_into, desc = 'Debug: Step Into' },
       { '<F2>', dap.step_over, desc = 'Debug: Step Over' },
       { '<F3>', dap.step_out, desc = 'Debug: Step Out' },
+      { '<leader>dt', dapui.toggle, desc = 'Debug: Toggle UI' },
+      { '<leader>dc', dap.continue, desc = 'Debug: Continue' },
+      { '<leader>dr', ":lua require('dapui').open({reset = true})<CR>", desc = 'Debug: Toggle UI' },
       { '<leader>b', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
       {
         '<leader>B',
