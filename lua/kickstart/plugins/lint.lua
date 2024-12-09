135e260ecbb8e999771fdc8bbca750d75edbf522
Stefanos Valoumas <svaloumas@gmail.com>
2024-12-09 11:00:33 +0200

    add noice

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        --markdown = { 'markdownlint' },
         go = { 'golangcilint' },
         python = { 'flake8' },
       }
